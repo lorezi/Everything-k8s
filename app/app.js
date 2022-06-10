b9c0518cac539a17d90724616009d8bd6b53e711
Lawrence Onaulogho <onaulogholawrence@gmail.com>
2022-06-10 11:09:02 +0100

    adds k8s manifest for probe

--- app/app.js
+++ app/app.js
@@ -3,18 +3,10 @@ const os = require('os');
 
 console.log("Kubia server starting...");
 
-var requestCount = 0
-
 var handler = function(req, res) {
-	console.log("Received request from " + request.connection.remoteAddress);
-  requestCount++;
-  if (requestCount > 5) {
-    response.writeHead(500);
-    response.end("I'm not well. Please restart me!");
-    return;
-  }
-  response.writeHead(200);
-  response.end("You've hit " + os.hostname() + "\n");
+	console.log("Received request from " + req.connection.remoteAddress);
+	res.writeHead(200);
+	res.end("You've hit " + os.hostname() + "\n")
 }
 
 var www = http.createServer(handler)
